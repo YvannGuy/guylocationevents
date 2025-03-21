ddc961dd1fae2536a001a50e769573df5d5c9336
yvann <yvann.guyonnet@gmail.com>
2025-03-21 15:32:19 +0100

    fix: correction des traductions et des erreurs dans l'API send-final-email

--- src/app/single-page/[slug]/page.jsx
+++ src/app/single-page/[slug]/page.jsx
@@ -5,11 +5,13 @@
  * Date: 2025-01-03
  */
 import * as React from "react";
-import PublicLayout from "@/_components/layouts";
 import { Fragment } from "react";
+import PublicLayout from "@/_components/layouts";
 import ProductDetailsInfo from "../components/details";
+
 const ProductDetails = async ({ params }) => {
-  const { slug } = params;
+  const slug = params.slug;
+  
   return (
     <Fragment>
       <PublicLayout>
